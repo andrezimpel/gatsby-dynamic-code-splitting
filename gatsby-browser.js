a6978d54cfd73904a1962eaa466f5ed7f4eafff3
Andre Zimpel <andre@unknownbasics.com>
2021-03-13 15:21:38 +0100

    gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,12 @@
-// import { hydrate, render } from 'react-dom';
-//
-// import { loadableReady } from '@loadable/component';
-//
-// export const replaceHydrateFunction = () => (element, container, callback) => {
-//   loadableReady(() => {
-//     // Using ReactDOM.hydrate on develop will throw an error in console
-//     const renderFn = process.env.GATSBY_BUILD_STAGE.includes('develop') ? render : hydrate;
-//
-//     renderFn(element, container, callback);
-//   });
-// };
+import { hydrate, render } from 'react-dom';
+
+import { loadableReady } from '@loadable/component';
+
+export const replaceHydrateFunction = () => (element, container, callback) => {
+  loadableReady(() => {
+    // Using ReactDOM.hydrate on develop will throw an error in console
+    const renderFn = process.env.GATSBY_BUILD_STAGE.includes('develop') ? render : hydrate;
+
+    renderFn(element, container, callback);
+  });
+};
