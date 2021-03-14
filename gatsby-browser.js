d276158173dd02ff605613a652758909cefd0497
Andre Zimpel <andre@unknownbasics.com>
2021-03-14 18:52:19 +0100

    gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,26 +1,10 @@
-import { hydrate, render } from 'react-dom';
+const ReactDOM = require('react-dom')
+const loadableReady = require('@loadable/component').loadableReady;
 
-import { loadableReady } from '@loadable/component';
-
-export const replaceHydrateFunction = (_, options) => (element, container, callback) => {
-  loadableReady(() => {
-    const renderFn = typeof options.useHydrate === 'undefined'
-        // Using ReactDOM.hydrate on develop will throw an error in console
-        ? process.env.GATSBY_BUILD_STAGE.includes('develop')
-            ? render
-            : hydrate
-        : !!options.useHydrate
-            ? hydrate
-            : render;
-
-    renderFn(element, container, callback);
-  });
+exports.replaceHydrateFunction = () => {
+  return (element, container, callback) => {
+    loadableReady(() => {
+        ReactDOM.render(element, container, callback);
+    });
+  };
 };
-
-
-// import { loadableReady } from '@loadable/component'
-//
-// loadableReady(() => {
-//   const root = document.getElementById('main')
-//   hydrate(<App />, root)
-// })
